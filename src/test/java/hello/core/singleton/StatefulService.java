c81fec4cd5bf088caec0e34612f48db4cf4c211a
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-07-01 15:29:03 +0900

    Resolved sharing same instance
    1. Delete private field and change void type to int type to return price.
    2. Creating local variable for each thread.

--- src/test/java/hello/core/singleton/StatefulService.java
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -1,12 +1,13 @@
 package hello.core.singleton;
 
 public class StatefulService {//어떤 statefulservice라는 서비스가 있다.
-    private int price;//상태 유지하는 필드. \
-    public void order(String name,int price){
+    //private int price;//상태 유지하는 필드. \
+    public int order(String name,int price){
         System.out.println("name = "+name+" price = "+price);
-        this.price = price;//여기가 문제!
+//        this.price = price;//여기가 문제!
+        return price;
     }
-    public int getPrice(){
-        return  price;
-    }
+//    public int getPrice(){
+//        return  price;
+//    }
 }
--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -16,15 +16,15 @@ public class StatefulServiceTest {
 
         //statefulService1,2는 같은 인스턴스인데 값을 넣어버리니까 값을 덮어씐다!
         //ThreadA : A사용자 10000원 주문
-        statefulService1.order("userA",10000);
+        int userAPrice = statefulService1.order("userA",10000);
         //ThreadB : B사용자 20000원 주문
-        statefulService2.order("userB",20000);
+        int userBPrice = statefulService2.order("userB",20000);
         
         //ThreadA: 사용자A 주문 금액 조회
-        int price = statefulService1.getPrice();
-        System.out.println("price = " + price);
+        //int price = statefulService1.getPrice();
+        System.out.println("price = " + userAPrice);
         //statefulService1의 금액이 20000원이냐?
-        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+//        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
     }
     static class TestConfig{
         @Bean

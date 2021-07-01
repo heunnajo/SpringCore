b76515b8c06b56cfd1746da6f00cbdbccd73268c
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-07-01 16:18:41 +0900

    Logging methods in AppConfig
    Expected : 3 times call of memberRepository, One time for memberService, orderService
    Real : Only one time for each method

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -14,16 +14,29 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig{
+    //예상 로그 결과 : memberRepository가 총 3번 호출되야한다!
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+    //call AppConfig.memberRepository
+
+    //실제 결과 : 스프링이 어떻게든 싱글톤을 보장해주는 구나!
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());//객체 생성하고 구현체 할당하는 것을 안에서 직접했었다.
     }
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());//객체 생성하고 구현체 할당하는 것을 안에서 직접했었다.
     }
     @Bean

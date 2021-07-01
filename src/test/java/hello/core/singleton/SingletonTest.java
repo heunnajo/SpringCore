5d8171820b0f26dc587ba32ada47bcd72ab9b4b6
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-07-01 12:54:34 +0900

    Update SingletonTest.java
    Test Singleton code.
    It returns same instance value meaning same instance!

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -6,6 +6,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class SingletonTest {
     //수많은 클라이언트의 요청에 대해 객체를 무수히 생성하면 어떻게 될까?
     //JVM 메모리에 계속 객체가 생성되어 올라간다!
@@ -20,8 +22,19 @@ public class SingletonTest {
 //        System.out.println("memberService1 = " + memberService1);
 //        System.out.println("memberService2 = " + memberService2);
         //memberService1 != memberService2 테스트자동화!
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
     }
-
+    @Test
+    @DisplayName("싱글톤 패턴 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService instance1 = SingletonService.getInstance();
+        SingletonService instance2 = SingletonService.getInstance();
+        System.out.println("instance1 = " + instance1);
+        System.out.println("instance2 = " + instance2);
+        //인스턴스가 같은지 비교하기위해 isSameAs 사용한다!
+        assertThat(instance1).isSameAs(instance2);
+        //same == : 객체 인스턴스가 같은지 참조값을 비교.
+        //equal : 자바의 equals
+    }
 
 }

349d23d604aa8035be5f5d555d28bae350b7d061
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-07-02 18:55:43 +0900

    Create SingletonWithPrototypeTest1.java
    Spring uses Singleton bean default, but when it gets Prototype bean via constructor DI, Prototype bean will be created but Singleton bean will have same Prototype bean as same as Singleton.

---
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,72 @@
+package hello.core.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonWithPrototypeTest1 {
+    @Test
+    void prototypeFind(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        prototypeBean1.addCount();
+        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        prototypeBean2.addCount();
+        assertThat(prototypeBean2.getCount()).isEqualTo(1);
+    }
+    @Test
+    void singletonClientUsePrototype(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class,PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(2);
+    }
+    @Scope("singleton")
+    static class ClientBean{
+        private final PrototypeBean prototypeBean;//생성시점에 prototypeBean이 주입된다!!!
+        //싱글톤인 ClientBean를 여러번 호출하더라도 이것은 하나의 동일한 prototypeBean이 된다.
+        @Autowired//prototype 내놔. => 스프링 컨테이너가 DI(prototype) 넣어준다.
+        public ClientBean(PrototypeBean prototypeBean){
+            this.prototypeBean = prototypeBean;
+        }
+        public int logic(){//client1과 client2 둘다 같은 prototypeBean을 쓴다!
+            prototypeBean.addCount();//이미 생성된 39번째 줄의 prototypeBean을 쓴다!
+            int count = prototypeBean.getCount();
+            return count;
+        }
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+
+        public void addCount(){
+            count++;
+        }
+        public int getCount(){
+            return count;
+        }
+        @PostConstruct
+        public void init(){
+            System.out.println("PrototypeBean.init" +this);//this=현재 나 자신(나 자신의 참조값)
+        }
+        @PreDestroy
+        public void destroy(){
+            System.out.println("PrototypeBean.destroy");
+        }
+
+    }
+}

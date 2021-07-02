f2b14925ad115b257e670e4c1e21e20148e928ae
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-07-02 17:25:25 +0900

    Singleton Scope Test

---
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,38 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SingletonTest {
+    @Test
+    void singletonBeanFind(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+        //검증 : isSameAs ==
+        assertThat(singletonBean1).isSameAs(singletonBean2);
+        ac.close();
+    }
+    //싱글톤 빈 설정정보 하나 만든다. - 싱글톤인 경우 디폴트라 생략가능하지만 학습용!
+    @Scope("singleton")
+    static class SingletonBean {
+        @PostConstruct
+        public void init(){
+            System.out.println("SingletonBean.init");
+        }
+        @PreDestroy
+        public void destroy(){
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+}

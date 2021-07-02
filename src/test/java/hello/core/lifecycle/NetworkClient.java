a87877c35150aae4cbbe26611ae81443d7686ce3
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-07-02 15:57:45 +0900

    Update NetworkClient.java
    Implement Spring Interface InitializingBean(afterPropertiesSet()), DisposableBean(destroy())

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,12 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {//이름 그대로 초기화해주는 인터페이스을 상속받는다!
     private String url;
 
     public NetworkClient(){
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
+
     }
     public void setUrl(String url){
         this.url = url;
@@ -21,5 +23,20 @@ public class NetworkClient {
     //서비스 종료 시 호출
     public void disconnect(){
         System.out.println("close: " + url);
+    }
+
+    //말 그대로 property들 셋팅이 끝나면(DI;의존관계 주입 끝나면)
+    //DI 끝나면 아래 함수 호출해준다!!!
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+    //빈이 종료될 때 아래 destroy()호출,실행!
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
